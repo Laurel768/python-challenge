ed84646627a75db2dc782363f7a2e72d0cb5b28e
Laurel768 <laurel.whitten@hotmail.com>
2020-09-15 19:46:28 -0400

    added average change in profit or loss

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,9 +7,16 @@ import csv
 #csv_file = os.path.join("folder_name", "file.csv")
 csvpath = os.path.join("Resources", "budget_data.csv")
 
+# define variables as integers
 total_months = 0
 total_profit_or_loss = 0
+change_in_profit_or_loss = 0
+prior_month__change_profit_or_loss = 0
+total_change_in_profit_or_loss = 0
 average_change = 0
+month_change_profit_or_loss = 0
+total_month_change = 0
+average_months = 0
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -19,8 +26,17 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         total_months += 1
         total_profit_or_loss +=int(row[1])
-        average_change = total_profit_or_loss/total_months
+        if total_months > 1:
+            month_change_profit_or_loss = int(row[1]) - prior_month_change_profit_or_loss
 
+        total_month_change += month_change_profit_or_loss 
+
+        prior_month_change_profit_or_loss = int(row[1])
+
+        average_change = total_month_change / total_months
+  
+
+       
 
 
  
@@ -28,7 +44,12 @@ print ("Financial Analysis")
 print ("----------------------------")
 print("Total Months: " +str(total_months))
 print("Total: $" + str(total_profit_or_loss))
-print("Average Change: $" + str(average_change))
+print(total_month_change)
+print(average_change)
+print(average_months)
+print("Greatest increase in profits: $")
+print("Greatest decrease in profits: $")
+
 
 
 
