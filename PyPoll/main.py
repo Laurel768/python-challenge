8d8cfe79d678be5a0175893daded7ed9027f035e
Laurel768 <laurel.whitten@hotmail.com>
2020-09-15 18:54:11 -0400

    added total votes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,3 +3,23 @@ import csv
 
 csvpath = os.path.join("Resources", "election_data.csv")
 
+total_votes = 0
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    csv_header = next(csvreader)
+
+    for row in csvreader:
+        total_votes +=1
+
+
+
+
+print("Election Results")
+print("-------------------------")
+print("Total Votes: " + str(total_votes))
+print("-------------------------")
+print("-------------------------")
+print("Winner: ")
+print("-------------------------")
