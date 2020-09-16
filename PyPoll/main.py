d90b8873c01fc801df547ca695a6c04420ede6b8
Laurel768 <laurel.whitten@hotmail.com>
2020-09-16 17:50:40 -0400

    counting votes and write to text file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,14 +4,31 @@ import csv
 csvpath = os.path.join("Resources", "election_data.csv")
 
 total_votes = 0
+candidates = []
+count_votes = []
+
+
+
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
     csv_header = next(csvreader)
 
+# count the total number of voters
     for row in csvreader:
         total_votes +=1
+
+        candidate = row[2]
+        if candidate in candidates:
+            candidate_index = candidates.index(candidate)
+            count_votes[candidate_index] = count_votes[candidate_index] + 1
+        else:
+            candidates.append(candidate)
+            count_votes.append(1)
+
+                  
+
 
 
 
@@ -23,3 +40,12 @@ print("-------------------------")
 print("-------------------------")
 print("Winner: ")
 print("-------------------------")
+
+f = open("analysis.txt", "w")
+f.write("Election Results\n")
+f.write("-------------------------\n")
+f.write("Total Votes: " + str(total_votes) + "\n")
+f.write("-------------------------\n")
+f.write("-------------------------\n")
+f.write("-------------------------\n")
+f.close()
